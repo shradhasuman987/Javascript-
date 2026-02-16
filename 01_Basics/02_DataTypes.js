22869a53a7ed0a5b6ad02032ea7e27a9de09f752
Shradha Suman <84612663+shradhasuman987@users.noreply.github.com>
2026-02-16 16:31:40 +0000

    container config done

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // can be done in Browser but not in nodejs.
+
+console.log("Hitesh")
+
+
+let name = "Shradha"
+let age = 22
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
