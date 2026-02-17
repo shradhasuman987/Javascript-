b203a5fccb1dc9cc872d11cd3c7a3a281e5045ac
Shradha Suman <84612663+shradhasuman987@users.noreply.github.com>
2026-02-17 16:25:20 +0000

    container config done

---
+++ 01_Basics/04_comparisons.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0); //false
+console.log(undefined >= 0); //false
+console.log(undefined <= 0); //false
+console.log(undefined < 0); //false
+console.log(undefined > 0); //false
+console.log(undefined = 0); //0
+console.log(undefined === 0); //false
+
+// ===  (strict operation)
+
+console.log("2" === 2); //false
