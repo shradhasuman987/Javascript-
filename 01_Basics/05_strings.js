3889199afb67ce548e1e308f0c8edc995e476b52
Shradha Suman <84612663+shradhasuman987@users.noreply.github.com>
2026-02-18 16:32:41 +0000

    container config done

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,36 @@
+
+const name = "shradha"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('shradha-ss-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   shradha   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://shradha.com/shradha%20suman"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
