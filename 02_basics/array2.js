0ae220816909d118208d8e608a198a5752f47f3b
Shradha Suman <84612663+shradhasuman987@users.noreply.github.com>
2026-02-24 15:19:50 +0000

    container config done

--- 02_basics/array2.js
+++ 02_basics/array2.js
@@ -1,7 +1,29 @@
 const marvel_heros = ["Spiderman", "Ironman", "Thor", "Hulk", "Captain America"];
 const dc_heros = ["Batman", "Superman", "Wonder Woman", "Flash", "Green Lantern"];
 
-marvel_heros.push(dc_heros);
+//marvel_heros.push(dc_heros);
 //console.log(marvel_heros);
 //console.log(marvel_heros[5][0]);
 
+//const allHeros = marvel_heros.concat(dc_heros);
+//console.log(allHeros);
+
+//Spread func in arrays
+
+const allHeros = [...marvel_heros, ...dc_heros];
+//console.log(allHeros);
+
+const AnotherArr = [1, 2, 3, [4, 5 , 6], 7, [8, 9, 10]];
+
+const realAnotherArr = AnotherArr.flat(Infinity);
+//console.log(realAnotherArr);
+
+console.log(Array.isArray("shradha"))
+console.log(Array.from("shradha"))
+console.log(Array.from({name: "shradha"})) 
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3));
