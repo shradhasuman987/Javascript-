b4b5a87fd06ef333244c72533e74ad49a4676d01
Shradha Suman <84612663+shradhasuman987@users.noreply.github.com>
2026-02-24 14:50:30 +0000

    container config done

--- 02_basics/array.js
+++ 02_basics/array.js
@@ -35,4 +35,6 @@ console.log("B" , myArr);
 const myn2 = myArr.splice(1, 3);
 
 console.log(myn2);
+console.log("C" , myArr);
+
 
---
+++ 02_basics/array2.js
@@ -0,0 +1,7 @@
+const marvel_heros = ["Spiderman", "Ironman", "Thor", "Hulk", "Captain America"];
+const dc_heros = ["Batman", "Superman", "Wonder Woman", "Flash", "Green Lantern"];
+
+marvel_heros.push(dc_heros);
+//console.log(marvel_heros);
+//console.log(marvel_heros[5][0]);
+
