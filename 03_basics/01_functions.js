d613b668d38ccfb31ada1a27e54235d0f015193b
Shradha Suman <84612663+shradhasuman987@users.noreply.github.com>
2026-03-04 04:39:16 +0000

    container config done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -36,4 +36,40 @@ function loginUserMessage(username){
 }
 
 //console.log(loginUserMessage("shradha"))
-console.log(loginUserMessage())
+//console.log(loginUserMessage())
+
+function calculateCartPrice(val1 , val2, ...num1){ //... is called rest and spread operator
+    return num1
+}
+
+//console.log(calculateCartPrice(200, 400, 600))
+
+const user = {
+    username: "shradha" ,
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`username id ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+//handleObject(user)
+
+//____passing object directly
+
+handleObject({
+    username: "sam",
+    price: 999
+})
+
+//_____passing array
+
+const myNewArray = [200, 400, 600, 100]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+//console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400 , 500 , 1000]));
+
