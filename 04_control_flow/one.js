be109869d27fc720164581dc343b071b5efd7b25
Shradha Suman <84612663+shradhasuman987@users.noreply.github.com>
2026-03-10 18:17:41 +0000

    done

---
+++ 04_control_flow/one.js
@@ -0,0 +1,45 @@
+/* If statement
+
+if(true){    //will execute
+
+}
+
+if(false){   //will not execute
+  
+} */
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+/* if(2 === 2){
+    console.log("Executed");
+} */
+
+/* const temp = 23
+if( temp <50){
+    console.log("Executed")
+} */
+
+/* const temp = 78
+if(temp === 50){
+    console.log("equal to 50")
+}else{
+    console.log("greater than 50")
+} */
+
+/* const score = 200
+if(score >100){
+    const power = "fly"
+    //var power = "fly"   // Don't give syntax error
+    console.log(`User power: ${power}`); //will execute
+}
+console.log(`User power: ${power}`); //syntax error near unexpected token ``User power: ${power}`'
+ */
+
+
+
+/* const isUserLoggedIn = true
+if(isUserLoggedIn){
+
+} */
+
+
