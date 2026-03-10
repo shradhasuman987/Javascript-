524859adee5fd250565a96fc3a35370145159eab
Shradha Suman <84612663+shradhasuman987@users.noreply.github.com>
2026-03-10 18:30:12 +0000

    done

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -35,11 +35,32 @@ if(score >100){
 console.log(`User power: ${power}`); //syntax error near unexpected token ``User power: ${power}`'
  */
 
+/* const balance = 1000
+// if (balance > 500) console.log("test"), console.log("test2");
 
-
-/* const isUserLoggedIn = true
-if(isUserLoggedIn){
+if (balance < 500) {
+    console.log("less than 500");
+} else if (balance < 750) {
+    console.log("less than 750");
+    
+} else if (balance < 900) {
+    console.log("less than 750");
+    
+} else {
+    console.log("less than 1200");
 
 } */
 
+const UserLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(UserLoggedIn && debitCard){
+    console.log("Allow to buy course");
+}
+
+if(loggedInFromEmail || loggedInFromGoogle) {
+    console.log("User Logged In ");  
+}
 
